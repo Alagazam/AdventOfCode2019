05b507ac1de7dc8e0e3cf0ffbc089abf8bc07805
David Darj <zid@alagazam.net>
2019-12-27 19:18:56 +0100

    Day02 B solved

--- Day02/Day02.h
+++ Day02/Day02.h
@@ -77,9 +77,18 @@ namespace Day02
 	int64_t Solve_B()
 	{
 		auto	file = aoc::OpenInputFile("Day02\\Day02.txt");
-		auto	input = aoc::ReadInputInt(*file);
-		input[0][1] = 12;
-		input[0][2] = 2;
+		const auto	input = aoc::ReadInputInt(*file);
+		for (int64_t noun = 0; noun != 100; ++noun)
+		{
+			for (int64_t verb = 0; verb != 100; ++verb)
+			{
+				auto program = input[0];
+				program[1] = noun;
+				program[2] = verb;
+				auto res = execute(program);
+				if (res == 19690720) return noun * 100 + verb;
+			}
+		}
 		return 0;
 	}
 
